21390675be822752e1415c0547d8782c1581ea46
pedro4064 <pedrohlcruz@gmail.com>
2020-04-30 11:09:56 -0300

    Creating a script to add anime priously donwloaded(in case of db falure)

---
+++ Add_Previously_Downloaded_Anime.py
@@ -0,0 +1,16 @@
+import Anime_NAS
+
+def logo():
+
+    logo_ascii = """
+
+    """
+
+def get_user_input():
+
+    anime_title = input('•Anime title- ')
+    number_of_episodes = input('•Number of episodes downloaded- ')
+    anime_id = ''
+
+    # Create a dict containing all the info
+    
